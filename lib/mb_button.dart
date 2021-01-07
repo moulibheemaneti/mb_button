221ed6fa52c70b5b018c88c21ec9dca5d4fd2ef0
moulibheemaneti <moulibheemaneti99@gmail.com>
2021-01-07 13:33:55 +0530

    first commit

--- lib/mb_button.dart
+++ lib/mb_button.dart
@@ -1,7 +1,52 @@
 library mb_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class MBButton extends StatefulWidget {
+  final String text;
+  final Color textColor;
+  final Color buttonColor;
+  final Icon icon;
+  final Function onTapFunction;
+  MBButton(
+      {@required this.text,
+      this.textColor,
+      this.buttonColor,
+      this.icon,
+      @required this.onTapFunction})
+      : assert(text != null);
+  @override
+  _MBButtonState createState() => _MBButtonState();
+}
+
+class _MBButtonState extends State<MBButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      padding: EdgeInsets.fromLTRB(35.0, 12.0, 35.0, 12.0),
+      minWidth: 120,
+      buttonColor:
+          widget.buttonColor != null ? widget.buttonColor : Colors.blue,
+      child: new RaisedButton(
+        elevation: 5.0,
+        textColor: widget.textColor != null ? widget.textColor : Colors.white,
+        onPressed: () {
+          // this function is called when the button is clicked
+          widget.onTapFunction;
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: new Text(
+          widget.text,
+          style: TextStyle(
+            fontSize: 20.0,
+            letterSpacing: 1.0,
+          ),
+        ),
+        color: Colors.brown,
+      ),
+    );
+  }
 }
